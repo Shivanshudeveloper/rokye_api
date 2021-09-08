ee911e95e3e1fe8a0c5824f7d8392ea1bd0f2692
shivanshu_gupta1 <shivanshu981@gmail.com>
2021-09-08 15:52:53 +0530

    Please work

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-//module.exports = {
-//MongoURI: 'mongodb+srv://shivanshu:shivanshu@1234@cluster0.t5khm.mongodb.net/ejouma?retryWrites=true&w=majority'
-//}
-
- module.exports = {
-   MongoURI: 'mongodb://localhost:27017/search'
+module.exports = {
+MongoURI: 'mongodb+srv://testadmin:1234567890@cluster0.6pvbu.mongodb.net/rokye?retryWrites=true&w=majority'
 }
+
+// module.exports = {
+//    MongoURI: 'mongodb://localhost:27017/search'
+// }
